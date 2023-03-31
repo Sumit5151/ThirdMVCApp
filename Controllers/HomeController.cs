fe4b11a3d36bb8dfa60340eff0b1f2b36152733d
Sumit5151 <sumitshitole5151@gmail.com>
2023-03-31 08:32:39 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ThirdMVCApp.Models;
+
+namespace ThirdMVCApp.Controllers
+{
+    public partial class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            SecondMvcappDbContext db = new SecondMvcappDbContext();
+              var users= db.Users.ToList();
+
+            return View(users);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Edit()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ Models/SecondMvcappDbContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace ThirdMVCApp.Models;
+
+public partial class SecondMvcappDbContext : DbContext
+{
+    public SecondMvcappDbContext()
+    {
+    }
+
+    public SecondMvcappDbContext(DbContextOptions<SecondMvcappDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("server=SUMIT; database=SecondMVCAppDB; trusted_connection=true; trustservercertificate=true;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Users__3214EC07056D6130");
+
+            entity.Property(e => e.Gender)
+                .HasMaxLength(10)
+                .IsUnicode(false);
+            entity.Property(e => e.Name)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
